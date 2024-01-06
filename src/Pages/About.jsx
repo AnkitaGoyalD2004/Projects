cc660f983f863a284dccd3e08c9ac335aadf9c88
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-06 13:27:00 +0530

    done

---
+++ src/Pages/About.jsx
@@ -0,0 +1,25 @@
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text 4-xl fold-bold leading-none tracking-tight sm:text-6xl">
+          WE LOVE
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              Comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quae
+        quam blanditiis vitae, dolor non eveniet ipsum voluptatibus, quia optio
+        aut! Perferendis ipsa cumque ipsam nostrum reprehenderit ad illo sed
+        officiis ea tempore! Similique eos minima sit porro, ratione aspernatur!
+      </p>
+    </>
+  );
+};
+export default About;
