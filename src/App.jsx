f38264278aa2137ee0717b130f6845731e9e7dcc
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-07 14:57:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./Pages";
 const App = () => {
-  return <div>App</div>;
+  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
 };
 export default App;
