f86a0f540a9e8716977d59a97573988891827420
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-01-07 16:09:45 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import {
+  RouterProvider,
+  UNSAFE_DataRouterStateContext,
+  createBrowserRouter,
+} from "react-router-dom";
+import {
   HomeLayout,
   Landing,
   Error,
@@ -11,7 +16,53 @@ import {
   Checkout,
   Orders,
 } from "./Pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "products/:id",
+        element: <SingleProduct />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      { path: "about", element: <About /> },
+      {
+        path: "checkout",
+        element: <Checkout />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
 const App = () => {
-  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
+  return <RouterProvider router={router} />;
 };
 export default App;
